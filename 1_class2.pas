e4b89161a4cd9bba848597c1a808f85316b9ab6b
Gleb Novikov <gnw_vandz@mail.ru>
2023-02-15 01:51:15 +0300

    2 Задание 1 урока сделано

---
+++ 1_class2.pas
@@ -0,0 +1,17 @@
+program hm;
+
+{$APPTYPE CONSOLE}
+
+uses
+    SysUtils;
+
+//var
+//    num1 : single;
+//    num2 : single;
+
+begin
+
+    writeln('The result is: ' ,0.03 + 0.6);
+// Выводин 6.30...00(E-0001)* т.к. часть в скобках * отвечает за *1/10
+// Компьютер точно не любит выводить ноль в начале - почему не знаю
+    end.
