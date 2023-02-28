f0cdb272f923ce7cd5d45eda1148090d529be739
Aleksandr Vakulenko <sasha705@mail.ru>
2023-02-28 23:40:54 +0300

    code update, commentary added

--- 2_class.pas
+++ 2_class.pas
@@ -7,30 +7,43 @@ uses
     Math;
 var
     mas : array of integer;
-    I, a, b, ram, N, sum1 : integer;
+    i, a, b, ram, N, sum1 : integer;
 label
     Lab;
 begin
     sum1:=0;
     write('Enter N :');
     Readln(N);
-
     setlength(mas, N);
-    for I := 0 to high(mas) do  begin  // цикл для генерации слйчайного N-ого массива
+	
+    for i := 0 to high(mas) do  begin 
+		{OLD CODE:
         Lab:
-        ram:=random(high(mas)+2); // Почему-то нужно +2 иначе идёт
-           // write(' -',ram, '- ');    //    в бесконечный перебор
-            for a := 0 to I do if ram=mas[a] then goto Lab;
+		// почему-то нужно +2 иначе идет в бесконечный перебор
+        ram:=random(high(mas)+2); 
+            for a := 0 to i do if ram=mas[a] then goto Lab;
         mas[i]:=ram;
-
-        //write(mas[i], ' ');    // для проверки значений массива
+		}
+		
+		// NEW CODE:
+		Lab:
+			ram:=random(high(mas)+2); 
+			for a := 0 to i do
+				if ram = mas[a] then
+					goto Lab;
+			mas[i] := ram;
+		// just look how it is become better with
+		// this indents and newlines
+		// (code logic unmodified)
+		writeln(mas[i]);
     end;
-      for b := 0 to (high(mas) div 2) do begin   // цикл для действий с массивом
-            //if mas[b]>(high(mas)/2+1) then write(mas[b], ' ') // от задания 2.4
-            sum1:= sum1 + mas[b];
-      end;
-        write('Sum: ', sum1);
-
-
-
+	
+	// Why do you need "b" variable if ypu could use "a"?
+	// it is free since previous cycle
+	for b := 0 to (high(mas) div 2) do begin   // цикл для действий с массивом
+		//if mas[b]>(high(mas)/2+1) then write(mas[b], ' ') // от задания 2.4
+		sum1:= sum1 + mas[b];
+	end;
+	
+	write('Sum: ', sum1);
 end.
